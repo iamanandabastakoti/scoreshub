90e109d8d4f2490e771de71ea51321eaad06ecb1
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2024-03-25 15:06:59 +0545

    choosing fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Orbitron: ["Orbitron", "sans-serif"],
+        Anta: ["Anta", "sans-serif"],
+        Audiowide: ["Audiowide", "sans-serif"],
+        Prompt: ["Prompt", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+        MontserratAlternate: ["Montserrat Alternates", "sans-serif"],
+      },
+      colors: {
+        primaryBg: "#FFFFFF",
+        primaryText: "#000000",
+        navbarBg: "#511DC9",
+        brandColor: "#32127A",
+        buttonColor: "#8C00FF",
+        borderColor: "#C0C0C0",
+      },
+    },
   },
   plugins: [],
 };
