cf7f5889411ded9f59f5a2c7b11140875942c74e
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2024-04-01 17:34:38 +0545

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SingleLeague from './pages/SingleLeague';
+import Footer from './components/Footer';
 
 const App = () => {
   const [sidebar, setSidebar] = useState();
@@ -13,15 +14,16 @@ const App = () => {
   //disables scrolling the body when sidebar is visible
   sidebar ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
   return (
-    <div className='bg-brandColor text-primaryBg font-Prompt flex flex-col min-h-screen'>
+    <div className='bg-brandColor text-primaryBg font-Prompt flex flex-col'>
       <Navbar toggleSidebar={toggleSidebar} setSidebar={setSidebar} />
-      <div className='pt-16 px-2 h-full'>
+      <div className='pt-16 px-2 min-h-screen'>
         <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
         <Routes>
           <Route index element={<Home />} />
           <Route path='/league/:leagueID' element={<SingleLeague />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='min-h-16 bg-navbarBg flex justify-center items-center gap-1'>
+            <span className='text-lg'>&copy;</span>2024 ScoresHub - All Rights Reserved
+        </div>
+    )
+}
+
+export default Footer
